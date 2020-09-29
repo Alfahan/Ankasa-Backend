b28bb67b4b6d361b180c8acbdf59f1e7b761228b
Alfahan <ali.farhan160@gmail.com>
2020-09-29 17:48:45 +0700

    Location

--- src/routes/location.js
+++ src/routes/location.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const locationController = require('../controllers/location');
+const { authenticate, authorize } = require('../helpers/auth')
 
 const router = express.Router();
 
 router
-    .get('/getall', locationController.getall)
-    .get('/getdetail/:idlocation', locationController.getdetail)
-    .post('/insert', locationController.insert)
-    .patch('/update/:idlocation',locationController.update)
-    .delete('/delete/:idlocation', locationController.delete)
+    .get('/getall', authenticate, authorize,locationController.getall)
+    .get('/getdetail/:idlocation', authenticate, authorize, locationController.getdetail)
+    .post('/insert', authenticate, authorize, locationController.insert)
+    .patch('/update/:idlocation', authenticate, authorize,locationController.update)
+    .delete('/delete/:idlocation', authenticate, authorize, locationController.delete)
 
 module.exports = router;
